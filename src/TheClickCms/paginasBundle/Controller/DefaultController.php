93437d3aec6de3319028c9c27b8e959e14b71e6a
vmonsalve <vicente.monsalve@gmail.com>
2014-02-18 17:15:48 -0300

    rama develop

--- src/TheClickCms/paginasBundle/Controller/DefaultController.php
+++ src/TheClickCms/paginasBundle/Controller/DefaultController.php
@@ -63,7 +63,13 @@ class DefaultController extends Controller
 
     public function recibeFormularioAccesoAction(Request $data)
     {
-        $usuario = $data->request->get('firstName');
-        $password = $data->request->get('emailaddress');
+        $rut = $data->request->get('rut');
+        $contrasena = $data->request->get('password');
+
+        if ($rut == '' or $contrasena == '') {
+            return new Response('100');
+        }else{
+            return new Response('200');
+        }
     }
 }
