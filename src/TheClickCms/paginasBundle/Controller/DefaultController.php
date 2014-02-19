9b8b02f889e108249ee1ec61b64fd3fab45a96a0
vmonsalve <vicente.monsalve@gmail.com>
2014-02-19 18:38:23 -0300

    Formulario recibe setEmpresa problema

--- src/TheClickCms/paginasBundle/Controller/DefaultController.php
+++ src/TheClickCms/paginasBundle/Controller/DefaultController.php
@@ -3,8 +3,10 @@
 namespace TheClickCms\paginasBundle\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Session\Session;
+use TheClickCms\AdminBundle\Entity\Usuarios;
+
 
 class DefaultController extends Controller
 {
@@ -80,7 +82,23 @@ class DefaultController extends Controller
 
     public function vistaAccesoAction()
     {
-    	return $this->render('TheClickCmspaginasBundle:Default:acceso.html.twig');
+        $session = $this->getRequest()->getSession();
+        
+        $usuario = $session->get('nusuario');
+        $contrasena = $session->get('contrasena');
+
+        $em = $this->getDoctrine()->getManager();
+        $usuarios = $em->getRepository('TheClickCmsAdminBundle:Usuarios')->findOneBy(array('nusuario' => $usuario , 'contrasena' => $contrasena) );
+
+        if ($usuario) {
+            $persona = $usuarios = $em->getRepository('TheClickCmsAdminBundle:Usuarios')->findOneBy(array('nusuario' => $usuario) );
+            return $this->render('TheClickCmspaginasBundle:Default:acceso.html.twig', array('persona' => $persona ));    
+        }else{
+            return $this->render('TheClickCmspaginasBundle:Default:index.html.twig');    
+        }
+
+
+    	
     }
 
 
@@ -90,24 +108,46 @@ class DefaultController extends Controller
 
         if($request->getMethod() == 'POST') {
 
+            $session = $this->getRequest()->getSession();
+
             $usuario = $request->request->get('usuario');
             $contrasena = $request->request->get('clave');
 
-
             $em = $this->getDoctrine()->getManager();
-            $usuario = $em->getRepository('TheClickCmsAdminBundle:Usuarios')->findOneBy(array('nusuario' => $usuario , 'contrasena' => $contrasena) );
-       
+            $usuarios = $em->getRepository('TheClickCmsAdminBundle:Usuarios')->findOneBy(array('nusuario' => $usuario , 'contrasena' => $contrasena) );
 
-            if($usuario){
+            if($usuarios){
+                $session->set('nusuario', $usuario);
+                $session->set('contrasena', $contrasena);
                 return new Response(200);
             }else{
-
-                   return new Response(100);  
-              
+                return new Response(100);  
             }
-
+        }
     }
 
-}
+    public function recibeformularioEditarAction(Request $request)
+    {
+        //Recibiendo los parametros del formulario editar.
+        $id = $request->request->get('id');
+        $pais = $request->request->get('pais');
+        $nombre = $request->request->get('nombre');
+        $correo = $request->request->get('email');
+        $cargo = $request->request->get('cargo');
+        $empresa = $request->request->get('empresa');
+        //Conectamos con la base de datos.
+        $em = $this->getDoctrine()->getManager();
+        $usuario = $em->getRepository('TheClickCmsAdminBundle:Usuarios')->findOneBy(array('id'=>$id));
+        //Seteando los valores que vienen del formulario editar.
+        $usuario->setPais($pais);
+        $usuario->setNombre($nombre);
+        $usuario->setEmail($correo);
+        $usuario->setCargo($cargo);
+        $usuario->setEmpresa($empresa);
+        //Haciendo la edicion en la base de datos.
+        $em->merge($usuario);
+        $em->flush();
+        return new response('Usuario Editado');
+    }
 
 }
