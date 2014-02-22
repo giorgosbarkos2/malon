e481588225acfd3943f42eadc60cdb75baa27f4c
vmonsalve <vicente.monsalve@gmail.com>
2014-02-22 16:09:51 -0300

    Mostrando las actualizaciones de la base de dato.

--- src/TheClickCms/paginasBundle/Controller/DefaultController.php
+++ src/TheClickCms/paginasBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Session\Session;
 use TheClickCms\AdminBundle\Entity\Usuarios;
+use TheClickCms\AdminBundle\Entity\Actuali;
 
 
 class DefaultController extends Controller
@@ -92,7 +93,8 @@ class DefaultController extends Controller
 
         if ($usuario) {
             $persona = $usuarios = $em->getRepository('TheClickCmsAdminBundle:Usuarios')->findOneBy(array('nusuario' => $usuario) );
-            return $this->render('TheClickCmspaginasBundle:Default:acceso.html.twig', array('persona' => $persona ));    
+            $actualizacion = $em->getRepository('TheClickCmsAdminBundle:Actualizacion')->findAll();
+            return $this->render('TheClickCmspaginasBundle:Default:acceso.html.twig', array('persona' => $persona, 'actualizacion' => $actualizacion));    
         }else{
             return $this->render('TheClickCmspaginasBundle:Default:index.html.twig');    
         }
